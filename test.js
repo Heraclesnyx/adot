ed84601bc4cfdfc5a5be3a7cecff2a1f1e600c71
Reboul Anthony <anthony12041989@gmail.com>
2019-10-28 16:09:24 +0100

    mise en place de la comparaison des lat et lon

--- test.js
+++ test.js
@@ -3,12 +3,13 @@ const fs = require("fs");
 const csv = require("csv-parser");
 const app = express();
 
-const results = {};
 
 
 
 /*Ma route sur localhost:8000*/
 app.get('/', function (req, res) {
+	const results = {};
+
 	res.send(
 
 		/*Récupération du fichier .csv*/
@@ -16,58 +17,34 @@ app.get('/', function (req, res) {
 		.pipe(csv())
 		.on('data', data =>{ 
 
-			let indexName = data.lat + '-' + data.lon;
-			// console.log(results[indexName]);
-			let arr = {
-				lat : data.lat,
-				lon : data.lon
-			}
-			arr[data.event_type] = 1;
+			let indexName = data.lat + ',' + data.lon;
 
-			// let toto = results.push();
-			// console.log(arr);
+            if (results[indexName] !== undefined){
+                //  Récupérer le type d'évenement du data et vérifier si il est présent (oui: on incrémente; non: on ajoute la propriété avec une value = 1)
+                if (results[indexName][data.event] !== undefined)
+                    results[indexName][data.event]++;
+                else
+                    results[indexName][data.event] = 1;
+            } else {
+                let ad = {
+                    lat: data.lat,
+                    lon: data.lon
+                }
 
-			// for (let i = 0; i < indexName.length; i++) {
-			//     if (results[i] === undefined)
-			//       results[i] = {};
+                ad[data.event] = 1;
 
-			//     results[i].lat = results[indexName]
-			// }
-			console.log(results.push(arr));
-			if (results[indexName] !== undefined){
-   //      	//    Récupérer le type d'évenement du data et vérifier si il est présent (oui: on incrémente; non: on ajoute la propriété avec une value = 1)
-   			// results .= indexName;
-        	// console.log(results);
-    		}
-    		// else{
-            //    Ajouter la ligne formaté comme on le souhaite en set le type d'évenement à 1
-        	// }
+                //  Push into global object
+                results[data.lat + '-' + data.lon] = results;
+                // console.log(results);
+            }
     	})
 		.on('end', () => {
-			console.log(results);
+			// console.log(results);
 		})
-	)	
+	)
+	// res.send(results);
 });
 
-app.post('/adot', function(req, res) {
-	res.send(
-		fs.createReadStream('events.csv')
-		.pipe(csv())
-		.on('data', data => {
-
-			// arr = [lat= row.lat, lon= row.lon, event= row.event_type];
-			// console.log(arr);
-			// console.log(row);
-		})		
-		// .on('end', () => {
-		// 	console.log('CSV file successfully processed');
-		// })
-		)
-		
-	// );
-	// return res.status(200).send(arrs);
-		// return res.status(202).send('J\'envoie ma requête');
-});
 
 app.listen(8000, function () {
   console.log('Example app listening on port 8000')
