501c6efa4363cb89b8422865cd789fbbbb0a047e
Reboul Anthony <anthony12041989@gmail.com>
2019-11-05 14:25:58 +0100

    fichier csv stocker ds une variable

--- test.js
+++ test.js
@@ -4,40 +4,71 @@ const csv = require("csv-parser");
 const app = express();
 
 
+function distance(lat1, lon1, lat2, lon2, unit = 'K') {
+    var radlat1 = Math.PI * lat1/180
+    var radlat2 = Math.PI * lat2/180
+    var theta = lon1-lon2
+    var radtheta = Math.PI * theta/180
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    if (dist > 1) {
+        dist = 1;
+    }
+    dist = Math.acos(dist)
+    dist = dist * 180/Math.PI
+    dist = dist * 60 * 1.1515
+    if (unit=="K") { dist = dist * 1.609344 }
+    if (unit=="N") { dist = dist * 0.8684 }
+    return dist
+}
+
+// function sortProperties(obj)
+// {
+//     // convert object into array
+//     var sortable=[];
+//     for(var key in obj)
+//         if(obj.hasOwnProperty(key))
+//             sortable.push([key, obj[key]]); // each item is an array in format [key, value]
+
+//     // sort items by value
+//     sortable.sort(function(a, b)
+//     {
+//         return a[1]-b[1]; // compare numbers
+//     });
+//     return sortable; // array in format [ [ key1, val1 ], [ key2, val2 ], ... ]
+// }
 
 
 /*Ma route sur localhost:8000*/
 app.get('/', function (req, res) {
-	const results = {};
 
-	// res.setHeader('Content-Type', 'application/json');
-	// res.send(
-
-
+	let results = [];
 		/*Récupération du fichier .csv*/
 		fs.createReadStream('events.csv')
 		.pipe(csv())
-		.on('data', data =>{ 
+		.on('data', data =>{
 
-			let indexName = data.lat + '-' + data.lon;
+			let result = results.push(data);
 
-            if (results[indexName] !== undefined){
-                //  Récupérer le type d'évenement du data et vérifier si il est présent (oui: on incrémente; non: on ajoute la propriété avec une value = 1)
-                if (results[indexName][data.event_type] !== undefined)
-                    results[indexName][data.event_type]++;
-                else
-                    results[indexName][data.event_type] = 1;
-            } else {
-                let ad = {
-                    lat: data.lat,
-                    lon: data.lon
-                }
+			//add imp and click
+			// result.forEach(function(el) {
+			// 	results[el.name] = {
+			// 		lat: el.lat,
+			// 		lon: el.lon,
+			// 		name: el.name,
+			// 		impressions: 0,
+			// 		clicks: 0
+			// 	}
+			// });
 
-                ad[data.event_type] = 1;
+			// let distances = {};
 
-                //  Push into global object
-                results[data.lat + '-' + data.lon] = ad;
-            }
+			// result.forEach(function(el) {
+			// 	distances[el.name] = distance(data.lat, data.lon, el.lat, el.lon);
+			// });
+
+			// let sortedDistances = sortProperties(distances);
+			// let event = data.event_type === 'imp' ? 'impressions' : 'clicks',
+			// results[sortedDistances[0][0]][event]++;
     	})
 		.on('end', () => {
 			console.log(results);
